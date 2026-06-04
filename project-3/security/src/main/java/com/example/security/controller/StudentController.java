41217b006ca4f50e5b6c7bbe8aac6e0778a31063
Surya <suryatk2007@gmail.com>
2026-06-04 14:14:55 +0530

    Refactor StudentController to use ArrayList for students and add CSRF token endpoint

--- project-3/security/src/main/java/com/example/security/controller/StudentController.java
+++ project-3/security/src/main/java/com/example/security/controller/StudentController.java
@@ -6,14 +6,17 @@ import com.example.security.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.web.csrf.CsrfToken;
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @RestController
 public class StudentController {
-    List<Student> students = List.of(
+    List<Student> students = new java.util.ArrayList<>(List.of(
         new Student(1, "Suryatk", "Java"),
         new Student(2, "John", "Python"),
         new Student(3, "Alice", "JavaScript")
-    );
+    ));
     @GetMapping("/students")
     public List<Student> getStudents() {
         return students;
@@ -23,5 +26,9 @@ public class StudentController {
         students.add(std);
         return students;
     }
+    @GetMapping("csrf-token")
+    public CsrfToken csrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    } 
     
 }
